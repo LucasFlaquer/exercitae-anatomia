81a5665f299b499649d8d7186769d4e278be52f8
LucasFlaquer <lucas.flaquer@gmail.com>
2023-09-06 16:13:51 -0300

    topics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './index.css'
 import { Banner } from './components/Banner'
+import { Topics } from './components/Topics'
 // import { Features } from './components/Features'
 // import { Oportunity } from './components/Oportunity'
-// import { Topics } from './components/Topics'
 // import { WhoWeAre } from './components/WhoWeAre'
 
 export function App() {
   return (
     <div className="bg-[#E8D8CE]">
       <Banner />
-      {/* <Topics />
+      <Topics />
+      {/* 
       <Oportunity />
       <Features />
       <WhoWeAre /> */}
--- src/components/Topics.tsx
+++ src/components/Topics.tsx
@@ -1,30 +1,39 @@
+import { ByNowButton } from './BuyNowButton'
+
 export function Topics() {
+  const topics = [
+    'Eixos e planos de seccção do corpo humano;',
+    'Nomenclatura de todos os ossos do corpo humano e suas estruturas;',
+    'Porque as articulações são importantes para o corpo humano e suas classificações;',
+    'Ilustrações detalhadas e explicações didáticas sobre todas as estruturas;',
+    'Secção de revisão ao término de cada capítulo;',
+  ]
+
   return (
-    <div className="topics">
-      <p>
+    <div className="mt-10 container mx-auto pb-44 max-w-3xl">
+      <p className="text-xl mx-auto text-center">
         Nosso e-book possui{' '}
         <strong>ilustrações detalhadas e explicações claras</strong> sobre
         sistemas orgânicos, estruturas e funções, proporcionando aos estudantes
         uma base sólida de apoio para cursar com <strong>êxito</strong> a
         disciplina de anatomia durante a graduação.
       </p>
-      <h3>Você vai encontrar nesse e-book conteúdos como:</h3>
-      <ul>
-        <li>Eixos e planos de seccção do corpo humano;</li>
-        <li>
-          Nomenclatura de todos os ossos do corpo humano e suas estruturas;
-        </li>
-        <li>
-          Porque as articulações são importantes para o corpo humano e suas
-          classificações;
-        </li>
-        <li>
-          Ilustrações detalhadas e explicações didáticas sobre todas as
-          estruturas;
-        </li>
-        <li>Secção de revisão ao término de cada capítulo;</li>
+      <h3 className="text-[#921f3E] font-bold text-center text-lg mt-8">
+        Você vai encontrar nesse e-book conteúdos como:
+      </h3>
+      <ul className="mx-auto mt-4">
+        {topics.map((topic) => (
+          <li
+            className="bg-[#921f3E] mb-4 text-xl text-white w-fit p-2"
+            key={topic}
+          >
+            {topic}
+          </li>
+        ))}
       </ul>
-      <button>Compre agora!</button>
+      <div className="text-center">
+        <ByNowButton />
+      </div>
     </div>
   )
 }
