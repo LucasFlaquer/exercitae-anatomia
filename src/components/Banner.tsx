c5f099e8bb60c9d27c31f955ef7c38bf5d735f83
LucasFlaquer <lucas.flaquer@gmail.com>
2023-09-14 15:04:15 -0300

    banner responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,17 +5,17 @@ import Bones from '../assets/chest.png'
 export function Banner() {
   return (
     <div
-      className={`bg-[banner_bones] container mx-auto flex items-center py-10 justify-center gap-10 relative`}
+      className={`container px-4 bg-[banner_bones]  mx-auto flex items-center py-10 justify-center gap-10 relative flex-col lg:flex-row`}
     >
       <img
         src={Bones}
         alt=""
-        className="absolute max-h-[500px] left-0 mix-blend-multiply top-[2rem]"
+        className="z-0 absolute left-[-10rem] md:left-0 top-[10rem] md:top-[2rem] max-h-[500px] mix-blend-multiply"
       />
       <div className="max-w-lg">
-        <h1 className="text-[#921F3E] font-bold uppercase text-6xl mb-4">
+        <h1 className="text-[#921F3E] font-bold uppercase text-2xl md:text-6xl mb-4">
           Bem-vindo(a)
-          <small className="block text-2xl mt-4 font-semibold">
+          <small className="block  md:text-2xl mt-4 font-semibold">
             ao e-book &quot;anatomia para calouros&quot;
           </small>
         </h1>
@@ -29,10 +29,12 @@ export function Banner() {
           a <strong>apoiar</strong> e <strong>guiar</strong> você durante essa
           trajetória.
         </p>
-        <ByNowButton />
+        <div className="text-center md:text-left">
+          <ByNowButton />
+        </div>
       </div>
-      <figure className="border-8 border-[#921F3E] shadow-[8px_8px_0_1px_rgba(96,17,38,1)]">
-        <img src={MainEbook} alt="" className="max-w-md" />
+      <figure className="border-8 border-[#921F3E] shadow-[8px_8px_0_1px_rgba(96,17,38,1)] z-[1]">
+        <img src={MainEbook} alt="" className="max-w-[300px] md:max-w-md" />
       </figure>
     </div>
   )
